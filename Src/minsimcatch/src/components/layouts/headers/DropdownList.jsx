e65a53b5b8dd4db4200d708f160353f234baee37
JongJoo <tlzhsh2@dgu.ac.kr>
2024-05-29 14:26:09 +0900

    feat/dropdown list

---
+++ Src/minsimcatch/src/components/layouts/headers/DropdownList.jsx
@@ -0,0 +1,55 @@
+export const sortList = [
+  {
+    id: 0,
+    category: "최신순",
+    value: "current",
+  },
+  {
+    id: 1,
+    category: "인기순",
+    value: "popular",
+  },
+];
+
+export const contentList = [
+  {
+    id: 0,
+    category: "골라줘(전체)",
+    value: "total",
+  },
+  {
+    id: 1,
+    category: "뭐사지(구매)",
+    value: "buy",
+  },
+  {
+    id: 2,
+    category: "어디갈까(갈곳)",
+    value: "where",
+  },
+  {
+    id: 3,
+    category: "뭐하지(할것)",
+    value: "what",
+  },
+  {
+    id: 4,
+    category: "뭐먹지(음식)",
+    value: "food",
+  },
+  {
+    id: 5,
+    category: "뭐볼까(미디어)",
+    value: "movie",
+  },
+  {
+    id: 6,
+    category: "들어줘(상담)",
+    value: "listen",
+  },
+  {
+    id: 7,
+    category: "기타",
+    value: "etc",
+  },
+];
